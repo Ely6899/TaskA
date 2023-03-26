3809cd2289eb74407bbf4a1bda8738757ad3fb18
Ely6899 <elikozinetz@protonmail.com>
2023-03-26 18:18:02 +0300

    Added comments to tests and changed several exception checks

--- Test.cpp
+++ Test.cpp
@@ -11,54 +11,54 @@
 
 
 
-TEST_CASE("Player initialization"){
+TEST_CASE("Player initialization"){ //Shouldn't throw an exception if Initialized with a proper name.
     CHECK_NOTHROW(Player("Alice"));
     CHECK_NOTHROW(Player("Bob"));
 }
 
-TEST_CASE("Player initialization (failure)"){
-    CHECK_THROWS_AS(Player(""), std::string);
-    CHECK_THROWS_AS(Player(" "), std::string);
+TEST_CASE("Player initialization (failure)"){ //Should throw a string exception if player initialized with empty string / spaces.
+    CHECK_THROWS_AS(Player(""), std::invalid_argument);
+    CHECK_THROWS_AS(Player(" "), std::invalid_argument);
 }
 
-TEST_CASE("Player function tests tests"){
+TEST_CASE("Player function tests tests"){ //Check stacksize() and cardesTaken() are >=0
     Player player{"Ely"};
     CHECK(player.stacksize() >= 0);
     CHECK(player.cardesTaken() >= 0);
 }
 
-TEST_CASE("Game initialization"){
+TEST_CASE("Game initialization"){ //Shouldn't throw an exception if game is initialized with proper players.
     CHECK_NOTHROW(Game(Player("Alice"), Player("Bob")));
 }
 
-TEST_CASE("Game initialization failures"){
-    CHECK_THROWS_AS(Game(Player(""), Player("Ely")), std::string);
-    CHECK_THROWS_AS(Game(Player("Ely"), Player("")), std::string);
-    CHECK_THROWS_AS(Game(Player("Ely"), Player("Ely")), std::string);
+TEST_CASE("Game initialization failures"){ //Throw string exceptions according to wrong player initializations OR players with the same name.
+    CHECK_THROWS_AS(Game(Player(""), Player("Ely")), std::invalid_argument);
+    CHECK_THROWS_AS(Game(Player("Ely"), Player("")), std::invalid_argument);
+    CHECK_THROWS_AS(Game(Player("Ely"), Player("Ely")), std::invalid_argument);
 }
 
-TEST_CASE("Check no stats yet (stats, log, last turn, winner)"){
+TEST_CASE("Check no stats yet (stats, log, last turn, winner)"){ //Throw string exception when we call print functions before playing the game.
     Game game{Player("Ely"), Player("Bob")};
     CHECK_THROWS_AS(game.printStats(), std::string);
     CHECK_THROWS_AS(game.printLastTurn(), std::string);
     CHECK_THROWS_AS(game.printLog(), std::string);
-    CHECK_THROWS(game.printWiner());
+    CHECK_THROWS_AS(game.printWiner(), std::string);
 }
 
-TEST_CASE("No winner before game finish"){
+TEST_CASE("No winner before game finish"){ //Throw string exception when trying to print winner before game ended.
     Game game{Player("Ely"), Player("Bob")};
-    game.playTurn();
+    game.playTurn(); //Still no winner after a single turn.
 
-    CHECK_THROWS(game.printWiner());
+    CHECK_THROWS_AS(game.printWiner(), std::string);
 }
 
-TEST_CASE("Check game ending condition"){
+TEST_CASE("Check game ending condition"){ //Throw exception if we try to run a game after it has ended.
     Game game{Player("Ely"), Player("Bob")};
     CHECK_NOTHROW(game.playAll());
-    CHECK_THROWS(game.playAll()); //If game already ended, can't start a new one.
+    CHECK_THROWS(game.playAll());
 }
 
-TEST_CASE("Test game functions"){
+TEST_CASE("Test game functions"){ //Run a game fully, testing no exception was thrown during the process.
     Game game{Player("Ely"), Player("Bob")};
     CHECK_NOTHROW(game.playTurn());
     CHECK_NOTHROW(game.printLastTurn());
